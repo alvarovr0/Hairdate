f4a94e51cf5b841c63872d28c41c9652be3dcd3f
alvarovr0 <alvaro.40.lva@gmail.com>
2023-05-09 12:26:29 +0200

    empezar con el mantener usuario iniciado y que vaya a un fragment u otro

--- Hairdate/app/src/main/java/com/example/hairdate/MainActivity.java
+++ Hairdate/app/src/main/java/com/example/hairdate/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.hairdate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment;
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            fragment = new principal();
+        } else {
+            fragment = new menu_Peluquero();
+        }
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment);
+        fragmentTransaction.commit();
     }
 }
