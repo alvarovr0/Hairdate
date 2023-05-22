828aa0fa766b279f27ea49e0a06873f39d4d33de
alvarovr0 <alvaro.40.lva@gmail.com>
2023-05-22 20:39:03 +0200

    arreglos

--- Hairdate/app/src/main/java/com/example/hairdate/inicioSesion_Cliente.java
+++ Hairdate/app/src/main/java/com/example/hairdate/inicioSesion_Cliente.java
@@ -71,7 +71,7 @@ public class inicioSesion_Cliente extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_inicio_sesion__cliente, container, false);
+        View view = inflater.inflate(R.layout.fragment_inicio_sesion_cliente, container, false);
         return view;
     }
 
