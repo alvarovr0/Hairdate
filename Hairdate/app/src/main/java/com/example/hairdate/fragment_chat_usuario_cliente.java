6658953fc69da6c6491a9eb58e7f6b31b578b656
Fav003 <101264590+Fav003@users.noreply.github.com>
2023-05-23 12:15:07 +0200

    Aplicacion chat

--- Hairdate/app/src/main/java/com/example/hairdate/fragment_chat_usuario_cliente.java
+++ Hairdate/app/src/main/java/com/example/hairdate/fragment_chat_usuario_cliente.java
@@ -8,12 +8,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.content.Intent;
+import android.net.Uri;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link fragment_chat_usuario_cliente#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fragment_chat_usuario_cliente extends Fragment {
+public class fragment_chat_usuario_cliente<CircleImageView, RecyclerView> extends Fragment {
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -24,6 +47,22 @@ public class fragment_chat_usuario_cliente extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private CircleImageView fotoPerfil;
+    private TextView nombre;
+    private RecyclerView rvMensajes;
+    private EditText txtMensaje;
+    private Button btnEnviar;
+    private AdapterMensajes adapter;
+    private ImageButton btnEnviarFoto;
+
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
+    private static final int PHOTO_SEND = 1;
+    private static final int PHOTO_PERFIL = 2;
+    private String fotoPerfilCadena;
+    private int contentView;
 
     public fragment_chat_usuario_cliente() {
         // Required empty public constructor
@@ -50,10 +89,88 @@ public class fragment_chat_usuario_cliente extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        setContentView(R.layout.activity_main);
+
+        fotoPerfil = (CircleImageView) findViewById(R.id.fotoPerfil);
+        nombre = (TextView) findViewById(R.id.nombre);
+        rvMensajes = (RecyclerView) findViewById(R.id.rvMensajes);
+        txtMensaje = (EditText) findViewById(R.id.txtMensaje);
+        btnEnviar = (Button) findViewById(R.id.btnEnviar);
+        btnEnviarFoto = (ImageButton) findViewById(R.id.btnEnviarFoto);
+        fotoPerfilCadena = "";
+
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("chat");//Sala de chat (nombre)
+        storage = FirebaseStorage.getInstance();
+
+        adapter = new AdapterMensajes(this);
+        LinearLayoutManager l = new LinearLayoutManager(this);
+        rvMensajes.setLayoutManager(l);
+        rvMensajes.setAdapter(adapter);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                databaseReference.push().setValue(new MensajeEnviar(txtMensaje.getText().toString(),nombre.getText().toString(),fotoPerfilCadena,"1", ServerValue.TIMESTAMP));
+                txtMensaje.setText("");
+            }
+        });
+
+        btnEnviarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.setType("image/jpeg");
+                i.putExtra(Intent.EXTRA_LOCAL_ONLY,true);
+                startActivityForResult(Intent.createChooser(i,"Selecciona una foto"),PHOTO_SEND);
+            }
+        });
+
+        fotoPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.setType("image/jpeg");
+                i.putExtra(Intent.EXTRA_LOCAL_ONLY,true);
+                startActivityForResult(Intent.createChooser(i,"Selecciona una foto"),PHOTO_PERFIL);
+            }
+        });
+
+        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeInserted(int positionStart, int itemCount) {
+                super.onItemRangeInserted(positionStart, itemCount);
+                setScrollbar();
+            }
+        });
+
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                MensajeRecibir m = dataSnapshot.getValue(MensajeRecibir.class);
+                adapter.addMensaje(m);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -61,5 +178,42 @@ public class fragment_chat_usuario_cliente extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_chat_usuario_cliente, container, false);
+    }
+
+    //On activity result
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PHOTO_SEND && resultCode == RESULT_OK){
+            Uri u = data.getData();
+            storageReference = storage.getReference("imagenes_chat");//imagenes_chat
+            final StorageReference fotoReferencia = storageReference.child(u.getLastPathSegment());
+            fotoReferencia.putFile(u).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri u = taskSnapshot.getDownloadUrl();
+                    MensajeEnviar m = new MensajeEnviar("Kevin te ha enviado una foto",u.toString(),nombre.getText().toString(),fotoPerfilCadena,"2",ServerValue.TIMESTAMP);
+                    databaseReference.push().setValue(m);
+                }
+            });
+        }else if(requestCode == PHOTO_PERFIL && resultCode == RESULT_OK){
+            Uri u = data.getData();
+            storageReference = storage.getReference("foto_perfil");//imagenes_chat
+            final StorageReference fotoReferencia = storageReference.child(u.getLastPathSegment());
+            fotoReferencia.putFile(u).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri u = taskSnapshot.getDownloadUrl();
+                    fotoPerfilCadena = u.toString();
+                    MensajeEnviar m = new MensajeEnviar("Kevin ha actualizado su foto de perfil",u.toString(),nombre.getText().toString(),fotoPerfilCadena,"2",ServerValue.TIMESTAMP);
+                    databaseReference.push().setValue(m);
+                    Glide.with(MainActivity.this).load(u.toString()).into(fotoPerfil);
+                }
+            });
+        }
+    }
+
+    public void setContentView(int contentView) {
+        this.contentView = contentView;
     }
 }
