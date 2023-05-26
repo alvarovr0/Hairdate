af952b57759074addc6413db0e596065875a5354
Fav003 <101264590+Fav003@users.noreply.github.com>
2023-05-26 12:51:50 +0200

    Cambios layout perfil pelu
    Se cambian los layouts del perfil y  se coloca el boton al fondo

--- Hairdate/app/src/main/java/com/example/hairdate/perfil_peluqueria.java
+++ Hairdate/app/src/main/java/com/example/hairdate/perfil_peluqueria.java
@@ -2,11 +2,22 @@ package com.example.hairdate;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldPath;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +34,7 @@ public class perfil_peluqueria extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private FirebaseFirestore db;
 
     public perfil_peluqueria() {
         // Required empty public constructor
@@ -58,7 +70,35 @@ public class perfil_peluqueria extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_perfil_peluqueria, container, false);
+
+        getParentFragmentManager().setFragmentResultListener("requestKey", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle bundle) {
+                // Exporta los datos del fragment que hemos solicitado antes y muestra el nombre del usuario insertado
+                String result = bundle.getString("bundleKey");
+                Query query = db.collection("Peluqueria").whereEqualTo(FieldPath.documentId(), result);
+                query.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+
+                            public void onSuccess(QuerySnapshot querySnapshot) {
+                                // Iterar a través de los documentos
+                                for (DocumentSnapshot document : querySnapshot.getDocuments()) {
+                                    // Obtener el valor en concreto que necesitas
+                                    String direccion = document.getString("direccion");
+                                    //usuario.setText(nombreUsuario);
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w("MyApp", "Error al obtener los documentos: ", e);
+                            }
+                        });
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil_peluqueria, container, false);
+        return rootView;
     }
 }
--- Hairdate/app/src/main/java/com/example/hairdate/principal_cliente.java
+++ Hairdate/app/src/main/java/com/example/hairdate/principal_cliente.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -11,10 +12,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -74,6 +78,44 @@ public class principal_cliente extends Fragment {
         recyclerView.setAdapter(adapter);
 
         return rootView;
+    }
+
+
+    public void onItemClick(Peluqueria peluqueria) {
+        // Aquí rediriges a otro fragmento o realizas la acción que desees
+        Toast.makeText(getActivity(), "Se te va a redirigir a la peluería: " + peluqueria.getNombre(), Toast.LENGTH_SHORT).show();
+
+        Query query = db.collection("Peluqueria").whereEqualTo("direccion", peluqueria.getDireccion());
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    QuerySnapshot querySnapshot = task.getResult();
+                    if (querySnapshot != null && !querySnapshot.isEmpty()) {
+                        DocumentSnapshot documentSnapshot = querySnapshot.getDocuments().get(0);
+                        String documentId = documentSnapshot.getId();
+                        // Aquí tienes la ID del documento
+                        Log.d("Firestore", "ID del documento: " + documentId);
+
+                        Bundle result = new Bundle();
+                        result.putString("bundleKey",documentId);
+
+                        //Imprimir traza en el logCat
+                        Log.d("UID", String.valueOf(result));
+                        getParentFragmentManager().setFragmentResult("requestKey", result);
+
+                        Navigation.findNavController(getView()).navigate(R.id.action_principal_cliente_to_perfil_peluqueria);
+                    } else {
+                        // No se encontraron resultados
+                        Log.d("Firestore", "No se encontraron resultados para la consulta.");
+                    }
+                } else {
+                    // Error al ejecutar la consulta
+                    Log.e("Firestore", "Error al ejecutar la consulta: " + task.getException().getMessage());
+                }
+            }
+        });
+
     }
 
     @Override
