411234602fe45ccc9e816fdf8cfff7f683973d53
Dilya <cpo0002@alu.medac.es>
2023-05-29 19:27:34 +0200

    Añadida la parte de RamaCristian

---
+++ Hairdate/app/src/main/java/com/example/hairdate/model/Citas.java
@@ -0,0 +1,21 @@
+package com.example.hairdate.model;
+
+public class Citas {
+    String Fecha_Hora;
+
+    public Citas() {
+
+    }
+
+    public Citas(String Fecha_Hora) {
+        this.Fecha_Hora = Fecha_Hora;
+    }
+
+    public String getFecha_Hora() {
+        return Fecha_Hora;
+    }
+
+    public void setFecha_Hora(String Fecha_Hora) {
+        this.Fecha_Hora = Fecha_Hora;
+    }
+}
